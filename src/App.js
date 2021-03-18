51370e7c4643cde435fda0a29cd07b1df839c927
roxana abigail vicentelo <abigailvicentelo@gmail.com>
2021-03-18 20:33:58 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import Formulario from './components/Formulario';
+
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Formulario/>
+    </div>
+  );
+}
+//imr sfc
+export default App;
